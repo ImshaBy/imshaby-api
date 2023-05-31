77d11d6b5e3fa5283c4b483a03a2d3e150616664
Andrei Misan <andrei.misan@gmail.com>
2023-05-31 16:36:57 +0300

    add cors

--- iac/tf/api/cdn.tf
+++ iac/tf/api/cdn.tf
@@ -3,8 +3,8 @@ resource "yandex_cdn_origin_group" "api_cdn_group" {
   name = var.api_cdn_origin_group_name
   use_next = true
   origin {
-    source = "${yandex_alb_load_balancer.api_alb.listener[0].endpoint[0].address[0].external_ipv4_address[0].address}:80"
-#    source = "${yandex_compute_instance.api_app.network_interface.0.nat_ip_address}:80"
+#    source = "${yandex_alb_load_balancer.api_alb.listener[0].endpoint[0].address[0].external_ipv4_address[0].address}:80"
+    source = "${yandex_compute_instance.api_app.network_interface.0.nat_ip_address}:80"
   }
 }
 
