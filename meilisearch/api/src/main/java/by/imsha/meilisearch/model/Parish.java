74fbec1609abb0627f1cf0faf5f72c93a630d82c
slongtong <slongtong@mail.ru>
2023-12-25 19:34:56 +0300

    minimized meilisearch model

--- meilisearch/api/src/main/java/by/imsha/meilisearch/model/Parish.java
+++ meilisearch/api/src/main/java/by/imsha/meilisearch/model/Parish.java
@@ -2,10 +2,13 @@ package by.imsha.meilisearch.model;
 
 import lombok.Builder;
 
+/**
+ * Данные парафии (прихода)
+ * <p>
+ * В текущей реализации нет необходимости хранить что-то кроме id
+ *
+ * @param id идентификатор парафии
+ */
 @Builder
-public record Parish(
-        String key,
-        String name,
-        String shortName,
-        String imgPath) {
+public record Parish(String id) {
 }
