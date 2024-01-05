5dfcdf93a02e911fa12608a36c8e1e87f1a2ec00
slongtong <slongtong@mail.ru>
2024-01-05 10:50:41 +0300

    meilisearch reader temp commit

---
+++ meilisearch/reader/src/main/java/by/imsha/meilisearch/reader/MeilisearchReader.java
@@ -0,0 +1,10 @@
+package by.imsha.meilisearch.reader;
+
+import com.meilisearch.sdk.Client;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+public class MeilisearchReader {
+
+    private final Client client;
+}
