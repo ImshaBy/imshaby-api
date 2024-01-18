09587e1d666f6c6e2d5ee209506032a16934337d
slongtong <slongtong@mail.ru>
2024-01-18 17:08:00 +0300

    writer integrated into server

---
+++ server/src/main/java/by/imsha/rest/passwordless/send/InterceptingCodeSender.java
@@ -0,0 +1,17 @@
+package by.imsha.rest.passwordless.send;
+
+import lombok.Getter;
+
+/**
+ * Перехватчик кода, сохраняет значение, вместо отправки
+ */
+public class InterceptingCodeSender implements CodeSender {
+
+    @Getter
+    private String code;
+
+    @Override
+    public void send(String userIdentifier, String code) {
+        this.code = code;
+    }
+}
