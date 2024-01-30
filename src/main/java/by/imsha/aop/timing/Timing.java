3c92a018e6377f8418fa06f639300be8adfa6d54
Vlad <vlad.shereshov@gmai.com>
2024-01-30 21:36:47 +0300

    fix server timing

--- src/main/java/by/imsha/aop/timing/Timing.java
+++ src/main/java/by/imsha/aop/timing/Timing.java
@@ -1,22 +1,32 @@
 package by.imsha.aop.timing;
 
-import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
-@AllArgsConstructor
 public class Timing {
+    private Long currentTime;
     private Long time;
     private Integer level;
 
+    private static final int START_LEVEL = 0;
+    private static final int LEVEL_STEP = 1;
+
+    public Timing() {
+        time = 0L;
+        level = START_LEVEL;
+    }
+
     public void increaseLevel (){
-        level = level + 1;
+        if (level == START_LEVEL){
+            currentTime = System.currentTimeMillis();
+        }
+        level += LEVEL_STEP;
     }
 
     public void decreaseLevel (){
-        level = level - 1;
-        if (level == 0) {
-            time = System.currentTimeMillis() - time;
+        level -= LEVEL_STEP;
+        if (level == START_LEVEL) {
+            time += System.currentTimeMillis() - currentTime;
         }
     }
 }
--- src/main/java/by/imsha/aop/timing/TimingService.java
+++ src/main/java/by/imsha/aop/timing/TimingService.java
@@ -2,6 +2,7 @@ package by.imsha.aop.timing;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 public class TimingService {
 
@@ -9,7 +10,7 @@ public class TimingService {
 
     public static void startTime(String name) {
         Map<String, Timing> timingMap = timingThreadLocal.get();
-        Timing timing = timingMap.computeIfAbsent(name, n -> new Timing(System.currentTimeMillis(), 0));
+        Timing timing = timingMap.computeIfAbsent(name, n -> new Timing());
         timing.increaseLevel();
     }
 
@@ -21,10 +22,14 @@ public class TimingService {
 
     public static String getResultServerTimingAndRemove() {
         Map<String, Timing> timingMap = timingThreadLocal.get();
-        StringBuilder result = new StringBuilder();
-        timingMap.forEach((s, timing) -> result.append(s).append("=").append(timing.getTime()).append(";"));
+        String result = timingMap.entrySet().stream()
+                .sorted((r1, r2) ->
+                        r2.getValue().getTime().compareTo(r1.getValue().getTime())
+                )
+                .map(r -> r.getKey() + "=" + r.getValue().getTime() + ";")
+                .collect(Collectors.joining());
         timingThreadLocal.remove();
-        return result.toString();
+        return result;
     }
 
 }
