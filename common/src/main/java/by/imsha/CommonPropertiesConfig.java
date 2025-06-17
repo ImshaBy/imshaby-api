9d56b1f91788122a884dfc0b9523dac411feae3e
slongtong <slongtong@mail.ru>
2025-06-17 22:09:08 +0300

    configuration of optional properties

--- common/src/main/java/by/imsha/CommonPropertiesConfig.java
+++ common/src/main/java/by/imsha/CommonPropertiesConfig.java
@@ -1,6 +1,9 @@
 package by.imsha;
 
 import by.imsha.properties.CommonProperties;
+import by.imsha.properties.FusionAuthProperties;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -12,5 +15,19 @@ public class CommonPropertiesConfig {
     @ConfigurationProperties("app.imsha")
     public CommonProperties commonProperties() {
         return new CommonProperties();
+    }
+
+    @Bean
+    @ConfigurationProperties("fusion-auth")
+    @ConditionalOnProperty(name = "fusion-auth.authorization-token")
+    public FusionAuthProperties fusionAuthProperties() {
+        return new FusionAuthProperties();
+    }
+
+    @Bean
+    @ConditionalOnMissingBean
+    public FusionAuthProperties fusionAuthPropertiesStub() {
+        //FIXME для того, чтобы не пришлось конфигурировать fusionAuth там, где он не нужен
+        return new FusionAuthProperties();
     }
 }
