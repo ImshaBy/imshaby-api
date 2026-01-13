d0567402f64fcd19c4f8fdcc800bba5d94665dff
slongtong <slongtong@mail.ru>
2026-01-13 18:49:43 +0300

    fix security configuration

--- server/src/main/java/by/imsha/security/SecurityConfig.java
+++ server/src/main/java/by/imsha/security/SecurityConfig.java
@@ -6,12 +6,10 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
 import org.springframework.core.env.Environment;
-import org.springframework.core.env.Profiles;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -33,22 +31,10 @@ import java.util.List;
 
 @Configuration
 @EnableWebSecurity
+@EnableMethodSecurity(securedEnabled = true)
 @RequiredArgsConstructor
 @Slf4j
 public class SecurityConfig {
-
-    private static final String PRODUCTION_PROFILE = "prod";
-    private static final String QA_PROFILE = "qa";
-    private static final String LOCAL_PROFILE = "local";
-
-    /**
-     * Включаем работу с аннотациями только для профилей prod и qa
-     */
-    @Configuration
-    @Profile(value = {PRODUCTION_PROFILE, QA_PROFILE, LOCAL_PROFILE})
-    @EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true)
-    public static class SecurityAnnotationsEnableConfiguration {
-    }
 
     @Bean
     CorsConfigurationSource corsConfigurationSource(CorsConfiguration corsConfiguration) {
@@ -76,47 +62,44 @@ public class SecurityConfig {
                 .csrf(AbstractHttpConfigurer::disable)
                 .sessionManagement(sessionManagement -> sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
 
-        //защищаем только на prod и qa
-        if (environment.acceptsProfiles(Profiles.of(PRODUCTION_PROFILE, QA_PROFILE, LOCAL_PROFILE))) {
-            http.authorizeHttpRequests(authorizeHttpRequests -> authorizeHttpRequests
-                            //ping controller
-                            .requestMatchers(HttpMethod.GET, "/").permitAll()
-                            .requestMatchers(HttpMethod.POST, "/api/passwordless/start").permitAll()
-                            .requestMatchers(HttpMethod.POST, "/api/passwordless/login").permitAll()
-                            .requestMatchers(HttpMethod.GET, "/api/mass/week").permitAll()
-                            .requestMatchers(HttpMethod.GET, "/api/mass/map").permitAll()
-                            .requestMatchers(HttpMethod.GET, "/api/mass/week-indexed").permitAll()
-                            .requestMatchers(HttpMethod.GET, "/api/mass/parish-week").permitAll()
-                            .requestMatchers(HttpMethod.GET, "/api/parish/*/state").permitAll()
-                            .requestMatchers(HttpMethod.GET, "/hook/parish").permitAll()
-                            .requestMatchers(HttpMethod.GET, "/hook/mass").permitAll()
-                            .requestMatchers(HttpMethod.GET, "/status").permitAll()
-                            .requestMatchers("/swagger-ui/**", "/v3/api-docs/**", "/v3/api-docs.yaml").permitAll()
-                            .anyRequest().authenticated()
-                    )
-                    .oauth2ResourceServer(oauth2ResourceServer ->
-                            oauth2ResourceServer.jwt(Customizer.withDefaults())
-                    );
-
-
-            //при наличии API ключей добавляем фильтр для работы с ними
-            if (!CollectionUtils.isEmpty(imshaProperties.getApiKeys()) ||
-                    !CollectionUtils.isEmpty(imshaProperties.getInternalApiKeys())) {
-                http.addFilterBefore(
-                        new ApiKeyAuthenticationFilter(
-                                Collections.unmodifiableSet(
-                                        new HashSet<>(imshaProperties.getApiKeys())
-                                ),
-                                Collections.unmodifiableSet(
-                                        new HashSet<>(imshaProperties.getInternalApiKeys())
-                                )
-                        ),
-                        BearerTokenAuthenticationFilter.class
+        http.authorizeHttpRequests(authorizeHttpRequests -> authorizeHttpRequests
+                        //ping controller
+                        .requestMatchers(HttpMethod.GET, "/").permitAll()
+                        .requestMatchers(HttpMethod.POST, "/api/passwordless/start").permitAll()
+                        .requestMatchers(HttpMethod.POST, "/api/passwordless/login").permitAll()
+                        .requestMatchers(HttpMethod.GET, "/api/mass/week").permitAll()
+                        .requestMatchers(HttpMethod.GET, "/api/mass/map").permitAll()
+                        .requestMatchers(HttpMethod.GET, "/api/mass/week-indexed").permitAll()
+                        .requestMatchers(HttpMethod.GET, "/api/mass/parish-week").permitAll()
+                        .requestMatchers(HttpMethod.GET, "/api/parish/*/state").permitAll()
+                        .requestMatchers(HttpMethod.GET, "/hook/parish").permitAll()
+                        .requestMatchers(HttpMethod.GET, "/hook/mass").permitAll()
+                        .requestMatchers(HttpMethod.GET, "/status").permitAll()
+                        .requestMatchers("/swagger-ui/**", "/v3/api-docs/**", "/v3/api-docs.yaml").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .oauth2ResourceServer(oauth2ResourceServer ->
+                        oauth2ResourceServer.jwt(Customizer.withDefaults())
                 );
-                log.info("API-key authentication filter enabled!");
-            } else {
-                log.info("API-key authentication filter disabled!");
-            }
+
+
+        //при наличии API ключей добавляем фильтр для работы с ними
+        if (!CollectionUtils.isEmpty(imshaProperties.getApiKeys()) ||
+                !CollectionUtils.isEmpty(imshaProperties.getInternalApiKeys())) {
+            http.addFilterBefore(
+                    new ApiKeyAuthenticationFilter(
+                            Collections.unmodifiableSet(
+                                    new HashSet<>(imshaProperties.getApiKeys())
+                            ),
+                            Collections.unmodifiableSet(
+                                    new HashSet<>(imshaProperties.getInternalApiKeys())
+                            )
+                    ),
+                    BearerTokenAuthenticationFilter.class
+            );
+            log.info("API-key authentication filter enabled!");
+        } else {
+            log.info("API-key authentication filter disabled!");
         }
 
         return http.build();
